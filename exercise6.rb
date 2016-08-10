d76eafa4e44c343bc34c1e851ff14c0b6a2d404e
Thomas Spencer <t.spencer1990@hotmail.com>
2016-08-10 14:31:25 -0400

    Completed all questions

--- exercise6.rb
+++ exercise6.rb
@@ -23,8 +23,19 @@ puts grocery_list.length
 check_for_bananas = grocery_list.include?("bananas")
 
 if check_for_bananas == true
-  puts "You need to pick upbananas"
+  puts "You need to pick up bananas"
 else
   puts "You don't need to pick up bananas"
-
 end
+
+#Displaying second item in the list
+
+puts grocery_list[1]
+
+#Soring the list alphabetically
+
+puts grocery_list.sort
+
+#No longer need cheese
+
+grocery_list.delete_at(2)
